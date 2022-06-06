d5c97fc88f84c6ceb831c4c29f94abb0a0e00723
Seyedrasool Sadrieh <rasool.sadrieh@gmail.com>
2022-06-06 07:38:43 -0700

    updated

---
+++ src/components/customer/customer-register.js
@@ -0,0 +1,53 @@
+import axios from "axios";
+import { useRef } from "react";
+import { useNavigate } from "react-router-dom";
+
+export default function CustomerRegister() {
+
+    const navigate = useNavigate();
+    const url = "https://rossrestaurant.azurewebsites.net";
+    
+
+    const fnameInput = useRef();
+    const lnameInput = useRef();
+    const usernameInput = useRef();
+    const passwordInput = useRef();
+ 
+  
+
+    // async-await
+    async function register() {
+        // Whenever you are getting a useRefs value, make sure it's inside some function call. Otherwise it will
+        // error due to the refInput.current = undefined, meaning there is no .value available
+        const user = {
+            fname: fnameInput.current.value,
+            lname: lnameInput.current.value,
+            username: usernameInput.current.value,
+            password: passwordInput.current.value,
+            balance: 0,
+            isAdmin: false,
+            
+        };
+        try {
+            const response = await axios.post(`${url}/customers`, user);
+            console.log(response.data);
+            navigate("/dashboard");
+        } catch (error) {
+            console.error(error.response.data);
+            alert(error.response.data);
+        }
+    }
+
+    return (
+        <>
+            <h4>Hello, new customer please register below.</h4>
+            <input placeholder="Enter First Name" ref={fnameInput}></input>
+            <input placeholder="Enter Last Name" ref={lnameInput}></input>
+            <br></br>
+            <input placeholder="Enter Your Username" ref={usernameInput}></input>
+            <input type="password" placeholder="Enter Your Password" ref={passwordInput}></input>
+            <br></br>
+            <button onClick={register}>Sign Up</button>
+        </>
+    );
+}
---
+++ src/components/customer/customer-welcome.js
@@ -0,0 +1,9 @@
+import axios from "axios";
+
+export default function CustomerWelcome() {
+    return (
+        <>
+            <h1>Welcome to the Ross and Jerry's cuisine!</h1>
+        </>
+    );
+}
